3a5740cb31e4643dd5d16bf65903cf575ba5a4cc
asaxen24 <asaxen24@ncsu.edu>
2026-05-13 22:55:16 -0400

    add class method structure to the agent

--- src/agent.py
+++ src/agent.py
@@ -5,17 +5,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Agent:
-    
-    client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
-    message = client.messages.create(
-        model="claude-opus-4-7",
-        max_tokens=1000,
-        messages=[
-            {
-                "role": "user",
-                "content": input("Ask the agent a question: ")
-            }
+    def __init__(self):
+        self.client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+    def ask_question(self, question):
+        message = self.client.messages.create(
+            model="claude-opus-4-7",
+            max_tokens=1000,
+            messages=[
+                {
+                    "role": "user",
+                    "content": question
+                }
         ],
     )
-    print(message.content[0].text)
+        return message.content[0].text  
+
+
+agent = Agent()
+answer = agent.ask_question(input("Ask the agent a question: "));
+print("Agent response: ", answer)
+
+
+
